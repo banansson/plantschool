c33d26fbe7d5d905fda29d9f67b8cc6dac6f6035
Jan Olofsson <banansson@gmail.com>
2011-05-25 21:13:32 +0200

    added Squidler - a chrome extension

---
+++ squidler/content-parser.js
@@ -0,0 +1,1 @@
+document.write("Squidler was here!");
---
+++ squidler/context-menu.js
@@ -0,0 +1,33 @@
+var debug = true;
+
+function log(message) {
+  if (debug) { console.log(extensionName + " | " + message); }
+}
+
+var markupdump = { }
+markupdump.ContextMenu = function(/*optional*/ options) {
+  if (typeof options !== "undefined") {
+    // extract options
+  }
+}
+
+markupdump.ContextMenu.prototype = {
+  main: function() {
+    var menu = chrome.contextMenus.create({
+      "title"   : "Inspect with Squidler",
+      "contexts": ["all"],
+      "onclick" : this._onParse
+    });
+    return menu;
+  },
+
+  _onParse: function(info, tab) {
+    chrome.tabs.executeScript(tab.id, {"file": "content-parser.js"});
+  },
+
+}
+
+var app = new markupdump.ContextMenu();
+menu = app.main();
+
+log("created context menu (id:  " + menu + ")");
