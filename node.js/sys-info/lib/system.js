3a4116b605bb2259cbef25308a04784fe4cbd66a
Jan Olofsson <banansson@gmail.com>
2011-06-13 23:27:04 +0200

    using async extension to read data from multiple commands at once

--- node.js/sys-info/lib/system.js
+++ node.js/sys-info/lib/system.js
@@ -1,24 +1,57 @@
-var sys = require('sys');
-var sysinfo = module.exports;
-var exec = require('child_process').exec;
+/**
+ * Read system data using uname
+ *   What              Switch
+ *  ---------------------------------------------------------------------------
+ *   all               -a, --all                all information
+ *   kernelName        -s, --kernel-name        kernel name 
+ *   nodeName          -n, --nodename           network node hostname
+ *   kernelRelease     -r, --kernel-release     kernel release
+ *   kernelVersion     -v, --kernel-version     kernel version
+ *   machineName       -m, --machine            machine hardware name
+ *   processor         -p, --processor          processor type or "unknown"
+ *   hardwarePlatform  -i, --hardware-platform  hardware platform or "unknown"
+ *   operatingSystem   -o, --operating-system   operating system
+ *
+ **/
 
-sysinfo.getName = function(callback) {
+var sys   = require('sys');
+var async = require('async');
+var exec  = require('child_process').exec;
+
+var sysinfo = module.exports;
+
+sysinfo.kernelNameOne = function(callback) {
+  var cmd = exec('uname -s', function(error, stdout, stderr) {
+    callback(stdout);
+  });
+}
+
+sysinfo.kernelName = function(callback) {
+  var cmd = exec('uname -s', function(error, stdout, stderr) {
+    callback(stdout);
+  });
+}
+
+sysinfo.nodeName = function(callback) {
   var cmd = exec('uname -n', function(error, stdout, stderr) {
     callback(stdout);
   });
 }
 
-sysinfo.getAll = function(callback) {
-  var cmd = exec('uname -a', function(error, stdout, stderr) {
-    callback(stdout);
+sysinfo.getAllSync = function(resultCallback) {
+  async.parallel({
+    kernelName: function(callback) {
+      var cmd = exec('uname -s', function(error, stdout, stderr) {
+        callback(null, stdout);
+      });
+    },
+    nodeName: function(callback) {
+      var cmd = exec('uname -n', function(error, stdout, stderr) {
+        callback(null, stdout);
+      });
+    }
+  },
+  function(err, results) {
+    resultCallback(results);
   });
 }
-
-sysinfo.getEverything = function(callback) {
-  var model = { };
-  sysinfo.getName(function(data) {
-    model.name = data;
-    callback(model);
-  });
-}
-
--- node.js/sys-info/sys-info.server.js
+++ node.js/sys-info/sys-info.server.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var sysinfo = require('./lib/system.js');
+var sys = require('sys');
 require('ejs');
 var express = require('express');
 var app = express.createServer();
@@ -8,16 +9,18 @@ var port = 60606;
 
 app.set('view engine', 'ejs');
 app.set('view options', { layout: false });
+
 app.get('/', function(req, res) {
   console.log(req.method + ' ' +
     req.url +
     ' from ' + req.client.remoteAddress + ':' + req.client.remotePort + 
     ' [' + req.headers['user-agent'] + ']');
-  sysinfo.getEverything(function(model) {
-    res.render('main', {
-      locals: model
+    sysinfo.getAllSync(function(model) {
+      console.log(sys.inspect(model));
+      res.render('main', {
+        locals: model
+      });
     });
-  });
 });
 
 app.listen(port);
