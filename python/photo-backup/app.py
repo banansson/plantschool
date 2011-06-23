52ba6de0cd966a013b4afb1b5f31e4ab9fc5c1a2
Jan Olofsson <banansson@gmail.com>
2011-06-23 17:16:05 +0200

    listing contains filesize and a summary, taking dir as argument

--- python/photo-backup/app.py
+++ python/photo-backup/app.py
@@ -6,14 +6,20 @@
 ##
 
 import os
+import sys
 
-def sourceDir():
-  return '~/Temp/Photos'
+def usage(argv):
+  print "Usage: %s [dir]" % argv[0]
 
-if __name__ == '__main__':
-  source = os.path.expanduser(sourceDir());
+def main(argv):
+  try:
+    requested = argv[1]
+  except:
+    usage(argv)
+    sys.exit(2)
 
-  print 'Start...'
+  source = os.path.expanduser(requested);
+
   print 'Source: ' + source
 
   # Set the stage
@@ -23,12 +29,21 @@ if __name__ == '__main__':
   sourceFiles = []
   for root, dirs, files in os.walk(source):
     for name in files:
-      file = os.path.join(root, name)
-      sourceFiles.append(file)
+      current = os.path.join(root, name)
+      sourceFiles.append(current)
 
   # Investigate result
+  fileCount = len(sourceFiles)
+  totalSize = 0
   for filePath in sourceFiles:
-    print filePath
+    info = os.stat(filePath)
+    size = info.st_size
+    totalSize += size
+    megabytes = float(size) / (1024 * 1024)
+    print '%s %.1fMB' % (filePath, megabytes)
 
-  print '...end'
+  print '%d files %.1fMB (%d bytes)' % (fileCount, totalSize / (1024 * 1024), totalSize)
+
+if __name__ == '__main__':
+  main(sys.argv)
 
