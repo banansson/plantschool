2ed8ef6fda3e45135a44c8aa1f5f42d8cbd7b183
Jan Olofsson <banansson@gmail.com>
2011-06-24 10:21:38 +0200

    extracted weight calculation

--- python/photo-backup/app.py
+++ python/photo-backup/app.py
@@ -11,6 +11,21 @@ import sys
 def usage(argv):
   print "Usage: %s [dir]" % argv[0]
 
+def weight(size):
+  kilobyte = 1024
+  megabyte = kilobyte * 1024
+  terabyte = megabyte * 1024
+  petabyte = terabyte * 1024
+
+  if (size < kilobyte):
+    return (size, ' bytes')
+  if (size < megabyte):
+    return (size / kilobyte, 'kB')
+  if (size < terabyte):
+    return (size / megabyte, "MB")
+  if (size < petabyte):
+    return (size / terabyte, "GB")
+
 def main(argv):
   try:
     requested = argv[1]
@@ -34,15 +49,16 @@ def main(argv):
 
   # Investigate result
   fileCount = len(sourceFiles)
-  totalSize = 0
+  totalBytes = 0
   for filePath in sourceFiles:
     info = os.stat(filePath)
     size = info.st_size
-    totalSize += size
-    megabytes = float(size) / (1024 * 1024)
-    print '%s %.1fMB' % (filePath, megabytes)
+    totalBytes += size
+    megabytes, postfix = weight(size) # float(size) / (1024 * 1024)
+    print '%s %.1f%s' % (filePath, megabytes, postfix)
 
-  print '%d files %.1fMB (%d bytes)' % (fileCount, totalSize / (1024 * 1024), totalSize)
+  totalSize, totalSizePostfix = weight(totalBytes)
+  print '%d files %.1f%s (%d bytes)' % (fileCount, totalSize, totalSizePostfix, totalBytes)
 
 if __name__ == '__main__':
   main(sys.argv)
